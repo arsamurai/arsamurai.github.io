dbe2d92a7862822c702059b24f5dd4f610bc9d7b
Artur Musienko <58669927+arsamurai@users.noreply.github.com>
2022-01-18 13:05:33 +0200

    Delete M-Box directory

--- M-Box/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.burger').on("click", function() {
-        $('.menu__list').toggleClass('menu__list--active');
-        $(this).toggleClass('burger--active');
-    })
-
-    new WOW().init();
-});
